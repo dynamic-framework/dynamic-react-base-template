7e75d986c45cbf5dce6d417657acb38ed4ac4b30
Abê <agonzalez@modyo.com>
2024-02-12 13:51:00 -0300

    chore: setup conditional style

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { DContextProvider } from '@dynamic-framework/ui-react';
@@ -8,8 +9,10 @@ import './config/i18nConfig';
 import App from './App';
 import { SITE_LANG, VARS_CURRENCY } from './config/widgetConfig';
 
-import '@dynamic-framework/ui-react/dist/css/dynamic-ui.css';
-import './styles/base.scss';
+if (process.env.NODE_ENV === 'development') {
+  require('@dynamic-framework/ui-react/dist/css/dynamic-ui.css');
+}
+require('./styles/base.scss');
 
 const root = ReactDOM.createRoot(document.getElementById('widgetName') as Element);
 root.render(
