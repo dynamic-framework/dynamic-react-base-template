900e0871e32e5cc611afe17dc13ee4a974d29d1f
Eduard Baron <42986303+efbarong@users.noreply.github.com>
2024-10-30 09:11:33 -0500

    chore: add eslint rules for import declarations and maximum props per line in jsx elements (#70)

--- src/components/MyLogos.tsx
+++ src/components/MyLogos.tsx
@@ -7,7 +7,11 @@ export default function MyLogos() {
   return (
     <div className="d-flex gap-4 flex-column flex-md-row justify-content-center align-items-center mt-4">
       <ModyoLogo />
-      <DIcon icon="plus" size="2rem" className="text-muted" />
+      <DIcon
+        icon="plus"
+        size="2rem"
+        className="text-muted"
+      />
       <ReactLogo />
     </div>
   );
