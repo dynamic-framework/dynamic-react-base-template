597888178212e5d07fdbf52e84ff260c65d7e55c
Eduard Baron <ebaron@modyo.com>
2025-05-28 15:02:31 -0500

    fix: recognize jest types in test files in the editor

--- tests/App.spec.tsx
+++ tests/App.spec.tsx
@@ -1,30 +1,32 @@
+/// <reference types="@testing-library/jest-dom" />
+
 import * as DynamicFramework from '@dynamic-framework/ui-react';
 import {
   render,
   screen,
   waitFor,
 } from '@testing-library/react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
 
 import App from '../src/App';
 import { CONTEXT_CONFIG } from '../src/config/widgetConfig';
 
 jest.mock('@dynamic-framework/ui-react', () => {
   const originalModule: typeof DynamicFramework = jest.requireActual('@dynamic-framework/ui-react');
+
   return {
     ...originalModule,
     useDContext: jest.fn(),
   };
 });
 
-describe('App Component', () => {
+describe('<App />', () => {
   beforeEach(() => {
     (DynamicFramework.useDContext as jest.Mock).mockReturnValue({
       setContext: jest.fn(),
     });
   });
-  it('sets context with correct configuration', async () => {
+
+  it('Sets context with correct configuration', async () => {
     render(<App />);
 
     await waitFor(() => {
